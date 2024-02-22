cd1ec1bc401a33a484b3f6910e5d2765795024aa
noah-bar <noah.barberini@eduvaud.ch>
2024-02-22 10:33:36 +0100

    feat: add student crud

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -6,4 +6,38 @@ class StudentsController < ApplicationController
   def show
     @student = Student.find params[:id]
   end
+
+  def new
+    @student = Student.new
+  end
+
+  def create
+    @student = Student.new student_params
+
+    if @student.save
+      redirect_to @student
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  def edit
+    @student = Student.find(params[:id])
+  end
+
+  def update
+    @student = Student.find(params[:id])
+
+    if @student.update(student_params)
+      redirect_to @student
+    else
+      render :edit, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def student_params
+    params.require(:student).permit(:first_name, :last_name, :email, :city, :npa, :address)
+  end
 end
